c361b4a0836ae7121267e2577d6fb9d240db0586
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-02-25 20:42:36 +0200

    Add project files.

---
+++ SpeckBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SpeckBot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            var token = File.ReadAllText("token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
