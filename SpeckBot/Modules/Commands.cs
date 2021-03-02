0d45bf18415c1553b0858dfb322b005018d4e044
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-03-02 10:36:43 +0200

    Fix bitcoin price command

--- SpeckBot/Modules/Commands.cs
+++ SpeckBot/Modules/Commands.cs
@@ -1,30 +1,32 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 using Discord.Commands;
 
 namespace SpeckBot.Modules
 {
     public class Commands : ModuleBase<SocketCommandContext>
     {
-        [Command("hello")]
+        [Command("bitcoin")]
         public async Task Greeting()
         {
-            await ReplyAsync("Hello team");
+            await ReplyAsync($"The price of Bitcoin is currently {GetBitcoinPrice()}€");
         }
 
-        //private static double GetBitcoinPrice()
-        //{
-        //    string URI = String.Format(@$"https://blockchain.info/tobtc?currency=EUR&value={0}", 1);
+        private double GetBitcoinPrice()
+        {
+            string URI = String.Format(@"https://blockchain.info/tobtc?currency=EUR&value={0}", 1);
 
-        //    WebClient client = new WebClient
-        //    {
-        //        UseDefaultCredentials = true
-        //    };
+            WebClient client = new WebClient
+            {
+                UseDefaultCredentials = true
+            };
 
-        //    string data = client.DownloadString(URI);
+            string data = client.DownloadString(URI);
 
-        //    double result = 1 / Convert.ToDouble(data.Replace('.', ','));
+            double result = 1 / Convert.ToDouble(data.Replace('.', ','));
 
-        //    return result;
-        //}
+            return Math.Round(result, 2);
+        }
     }
 }
